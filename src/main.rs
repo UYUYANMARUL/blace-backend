c4cb160e1be99a98f60d3e844dd295f8d40eb3c5
UYUYANMARUL <ataberkcekic@hotmail.com.tr>
2025-09-16 16:30:22 +0300

    fix(api) the port address is changed to 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -79,7 +79,7 @@ async fn main() -> std::io::Result<()> {
             )
             .route("/ws/{game_id}", web::get().to(websocket::websocket_handler))
     })
-    .bind(("127.0.0.1", 8000))?
+    .bind(("0.0.0.0", 8000))?
     .run()
     .await
 }
